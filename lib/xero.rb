45b7574265e1dbb8a335b7f516b2ab9be4769e15
Joseph Weissman <joe@bignerdranch.com>
2017-03-06 14:56:44 -0500

    draft token-parse pipeline

--- lib/xero.rb
+++ lib/xero.rb
@@ -1,1 +1,177 @@
 require 'xero/version'
+
+module Xero
+  class Environment
+  end
+
+  class Token
+    attr_reader :content
+    def initialize(val)
+      @content = val
+    end
+  end
+
+  class Whitespace < Token
+    def self.pattern
+      /\s+/
+    end
+  end
+
+  class Label < Token
+    def self.pattern
+      /[a-zA-Z]+/
+    end
+  end
+
+  class OpToken < Token; end
+
+  class Arrow < OpToken
+    def self.pattern
+      /->/
+    end
+  end
+
+  class Colon < OpToken
+    def self.pattern
+      /:/
+    end
+  end
+
+  # class LeftParens ...
+
+  class Tokenizer
+    def analyze(string)
+      scanner = StringScanner.new(string)
+      tokens = []
+      until scanner.eos?
+        token_kinds.each do |token_kind|
+          matched_token = scanner.scan(token_kind.pattern)
+          if matched_token
+            tokens << token_kind.new(matched_token)
+            break
+          end
+        end
+      end
+      tokens #.reject { |token| token.is_a?(Whitespace) }
+    end
+
+    def token_kinds
+      [ Label, Arrow, Whitespace ]
+    end
+  end
+
+  class ExpressionNode
+    attr_reader :value, :left, :right
+    def initialize(value, left: nil, right: nil) #, children: [])
+      @value = value
+      @left = left
+      @right = right
+      # @children = children
+    end
+  end
+
+  class LabelNode < ExpressionNode; end
+  class OperatorNode < ExpressionNode; end
+
+  class Parser
+    # build ast and return root!
+    def analyze(tokens)
+      # toss whitespace out
+      tokens.reject! { |token| token.is_a?(Whitespace) }
+      puts "--- ANALYZE tokens=#{tokens}"
+      expression(tokens)
+    end
+
+    def expression(tokens)
+      # eventually, lists of ops -> defs; key/barewords -> cmds; lists of defs + cmds -> prog
+      if tokens.length == 1
+        the_token = tokens.first
+        return label(the_token) if label(the_token)
+      else
+        operation(tokens)
+      end
+    end
+
+    def label(token)
+      if token.is_a?(Label)
+        LabelNode.new(token.content)
+      end
+    end
+
+    def operator(token)
+      if token.is_a?(OpToken)
+        case token
+        when Arrow then :arrow
+        when Colon then :defn
+        end
+      end
+    end
+
+    def operation(tokens)
+      first, second, *rest = tokens
+      if label(first) && operator(second) && expression(rest)
+        OperatorNode.new(operator(second), left: label(first), right: expression(rest))
+      end
+    end
+  end
+
+  class Command; end
+  class HaulCommand < Command; end
+  class CommandResult; end
+  class SuccessfulCommand < CommandResult; end
+
+  class Interpreter
+    def analyze(ast)
+      # turn ast into command
+    end
+  end
+
+  class Processor
+    def initialize(environment)
+      @env = environment
+    end
+
+    def analyze(command)
+      # compute result of command in environment
+      # aggregate fired events for further processing...
+    end
+  end
+
+  class Repl
+    attr_reader :env
+    def initialize
+      @env = Environment.new
+      @tokenizer = Tokenizer.new
+      @parser = Parser.new
+      @interpreter = Interpreter.new
+      @processor = Processor.new(@env)
+    end
+
+    def launch!
+      puts welcome_message
+      print ">> "
+      xero_eval(gets.chomp)
+    end
+
+    protected
+
+    def welcome_message
+      "XERO #{Xero::VERSION}\n\n\n"
+    end
+
+    private
+
+    def xero_eval(command)
+      puts "[xero eval #{command}]"
+      tokens = @tokenizer.analyze(gets)
+      puts "tokens: #{tokens}"
+      ast = @parser.analyze(tokens)
+      puts "ast: #{ast}"
+      command = @interpreter.analyze(ast)
+      puts "commands: #{commands}"
+      result, events = @processor.analyze(command)
+      puts "result: #{result}"
+      puts "events: #{events}"
+    end
+  end
+end
--- spec/xero_spec.rb
+++ spec/xero_spec.rb
@@ -6,3 +6,64 @@ describe Xero do
     expect(subject.const_get('VERSION')).to_not be_empty
   end
 end
+
+describe Tokenizer do
+  let(:tokenizer) do
+    Tokenizer.new
+  end
+
+  it 'should break strings into tokens' do
+    tokens = tokenizer.analyze('hello -> world')
+    expect(tokens.length).to eq(5)
+    expect(tokens.first).to be_a(Label)
+    expect(tokens.first.content).to eq('hello')
+    expect(tokens.map(&:class)).to eq([Label, Whitespace, Arrow, Whitespace, Label])
+    expect(tokens.map(&:content)).to eq(['hello', ' ', '->', ' ', 'world'])
+  end
+end
+
+describe Parser do
+  let(:tokenizer) do
+    Tokenizer.new
+  end
+
+  let(:parser) do
+    Parser.new
+  end
+
+  describe 'assembles tokens into trees' do
+    it 'can analyze a simple operation' do
+      tokens = tokenizer.analyze('hello -> world')
+      ast = parser.analyze(tokens)
+      expect(ast).to be_a(ExpressionNode)
+      expect(ast).to be_a(OperatorNode)
+
+      node = ast
+      expect(node.left).to be_a(LabelNode)
+      expect(node.left.value).to eq('hello')
+
+      expect(node.value).to eq(:arrow)
+
+      expect(node.right).to be_a(LabelNode)
+      expect(node.right.value).to eq('world')
+    end
+
+    it 'can analyze a more complex operation' do
+      tokens = tokenizer.analyze('hello -> there -> world')
+      ast = parser.analyze(tokens)
+      expect(ast).to be_a(ExpressionNode)
+      expect(ast).to be_a(OperatorNode)
+
+      root_node = ast
+      expect(root_node.value).to eq(:arrow)
+      expect(root_node.left).to be_a(LabelNode)
+      expect(root_node.left.value).to eq('hello')
+
+      right_node = root_node.right
+      expect(right_node).to be_a(OperatorNode)
+      expect(right_node.value).to eq(:arrow)
+      expect(right_node.left.value).to eq('there')
+      expect(right_node.right.value).to eq('world')
+    end
+  end
+end
