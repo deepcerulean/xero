39892bf78507f1471122c9627c3422c84b1315cb
Joseph Weissman <joe@bignerdranch.com>
2017-03-16 11:04:09 -0400

    extract parser to own file

--- lib/xero.rb
+++ lib/xero.rb
@@ -1,76 +1,9 @@
 require 'xero/version'
 require 'xero/tokenizer'
+require 'xero/parser'
 require 'pry'
 
 module Xero
-  class ExpressionNode
-    attr_reader :value, :left, :right
-    def initialize(value, left: nil, right: nil)
-      @value = value
-      @left = left
-      @right = right
-    end
-
-    def to_s(depth: 1)
-      tabs = "\n" + ('  ' * depth)
-      if !(@left || @right)
-        tabs + "#{self.class.name}[#@value]"
-      else
-        tabs + "#{self.class.name}[#@value]\n" +
-          tabs + "left: #{@left.to_s(depth: depth+1)}\n" +
-          tabs + "right: #{@right.to_s(depth: depth+1)}\n"
-      end
-    end
-    alias :inspect :to_s
-  end
-
-  class LabelNode < ExpressionNode; end
-  class OperationNode < ExpressionNode; end
-
-  # TODO parse out multiple statements separated by semicolons...
-  # class StatementNode < ExpressionNode
-
-  class Parser
-    # take tokens, build ast -- and return root of the abstract syntax tree!
-    def analyze(tokens)
-      tokens.reject! { |token| token.is_a?(WhitespaceToken) }
-      expression(tokens)
-    end
-
-    protected
-    def expression(tokens)
-      if tokens.length == 1
-        the_token = tokens.first
-        return label(the_token) if label(the_token)
-      else
-        operation(tokens)
-      end
-    end
-
-    def label(token)
-      if token.is_a?(LabelToken)
-        LabelNode.new(token.content)
-      end
-    end
-
-    def operator(token)
-      if token.is_a?(OpToken)
-        case token
-        when ArrowToken then :arrow
-        when ColonToken then :defn
-        when DotToken then :dot
-        end
-      end
-    end
-
-    def operation(tokens)
-      first, second, *rest = tokens
-      if label(first) && operator(second) && expression(rest)
-        OperationNode.new(operator(second), left: label(first), right: expression(rest))
-      end
-    end
-  end
-
   ## command interpreter..
   class Command; end
   class QueryEntityCommand < Command
